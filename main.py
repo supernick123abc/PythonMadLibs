63c1a3f7e93f643f520ec73f83b76a8639feb823
Nick Messenger <supernick123abc@gmail.com>
2020-05-13 20:19:15 -0700

    Added variables so that files can be used

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 # Imports / Dependencies
 import re
 # Placeholder for full text story
-story = "A vacation is when you take a trip to some [adjective1] place with your [adjective2] family. Usually you go to some place that is near a/an [noun1] or up on a/an [noun2]. A good vacation place is one where you can ride [noun_plural1] or play [game] or go hunting for [noun-plural]. I like to spend my time [verb_ing1] or [verb_ing2]. When parents go on a vacation, they spend their time eating three [noun_plural2] a day, and fathers play golf, and mothers sit around [verb_ing3]. Last summer, my little brother fell in a/an [noun3] and got poison [noun4] all over his [body1]. My family is going to go to (the) [place1], and I will practice [verb_ing4]. Parents need vacations more than kids because parents are always very [adjective3] and because they have to work [number1] hours every day all year making enough [noun_plural3] to pay for the vacation."
+story_file = open("story.txt","r")
+story = story_file.read()
 story2 = ""
 
 def word_scan(): 
@@ -32,6 +33,11 @@ def word_scan():
     print("\n")
     print(story2)
     print("\n")
+    
+    story_file2 = open("story_modified.txt", "w+")
+    story_file2.write(story2)
+    story_file.close
+    story_file2.close
     
     # BELOW CODE ALLOWS FOR FORMATTING OF EACH MATCH
     # for matchNum, match in enumerate(matches, start=1):
